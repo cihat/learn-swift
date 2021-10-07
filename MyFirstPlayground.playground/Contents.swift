1ca470fcfb957bf4abf92ce249eb96479f997dcb
cihat <cihat02020202@gmail.com>
2021-10-07 22:41:01 +0300

    Add myFirstSwift file

---
+++ MyFirstPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var greeting: String = "2434"
+var selam: String = "This is a contant variable"
+selam = "asdlşkfjasfşlaksjdf"
+var hello = 32
+hello = 234
+
+var a: Int = 23
+var b: Int = 23424
+
+a = b
+
+var c: Float = 234.42423423423
+var d: Double = 2343432.2334
+var e: Bool = false
+var f: Decimal = 10101010
+
+
+e = true
+print(String(e))
+
+print(Int(c))
+print(Int(d))
+
+
+
+
+
+
+print(selam)
+print(greeting)
+print("salfkjsadlşfkjasdfşlaskdjf")
+print(hello)
